dab426c7b71413298f4ce36368fde6320cff95c4
SarahElgindy <sarah.elgindy970@gmail.com>
2024-10-21 01:12:00 +0300

    Updated

--- task_4.sql
+++ task_4.sql
@@ -2,6 +2,6 @@
 -- This script prints the full description of the 'Books' table
 
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
-FROM information_schema.columns
-WHERE table_name = 'Books'
-AND table_schema = DATABASE();
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = alx_book_store
+AND TABLE_NAME = 'Books'
