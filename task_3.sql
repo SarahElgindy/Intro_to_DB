7d3858ca06372242cca0ff39ad539b5f23a5a77c
SarahElgindy <sarah.elgindy970@gmail.com>
2024-10-21 01:05:11 +0300

    Updated

--- task_3.sql
+++ task_3.sql
@@ -1,4 +1,4 @@
 -- This script lists all the tables in the given database using the SHOW TABLES command.
 SELECT table_name 
 FROM INFORMATION_SCHEMA.tables 
-WHERE table_schema = 'alx_book_store';
+WHERE table_schema = USE alx_book_store;
