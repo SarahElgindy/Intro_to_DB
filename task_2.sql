50a3f6bdf96709e1044baf30d94984e9431d5011
SarahElgindy <sarah.elgindy970@gmail.com>
2024-10-21 00:48:41 +0300

    Updated

--- task_2.sql
+++ task_2.sql
@@ -22,11 +22,11 @@ CREATE TABLE IF NOT EXISTS customers (
     address TEXT
 );
 
--- Create the 'Orders' table
+-- Create the 'orders' table
 CREATE TABLE IF NOT EXISTS orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT,
-    order_date DATE,
+    customer_id INT NOT NULL,
+    order_date DATE NOT NULL,
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id) ON DELETE CASCADE
 );
 
