0f82103c2c4f7a09f8b34ff4a7be9c24d91ecf0d
SarahElgindy <sarah.elgindy970@gmail.com>
2024-10-21 01:14:30 +0300

    Updated

---
+++ task_5.sql
@@ -0,0 +1,5 @@
+-- task_5.sql
+-- This script inserts a single row into the 'customer' table.
+
+INSERT INTO customer (customer_id, customer_name, email, address) 
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
